4fc41725bf1e62a26c44c6f50e30221a67645296
Emmanuel Oare <54752731+Elvasil-tech@users.noreply.github.com>
2022-04-25 17:09:18 +0100

    Add files via upload

---
+++ components/Main.js
@@ -0,0 +1,19 @@
+import React from  "react"
+
+function Main(){
+    return (
+        <main className="container">
+            <h1 className="--h1-title">Fun facts about React </h1>
+            <ul className="--ul-item">
+                <li>was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has over 100k stars on Github</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+            <a href = "" className="button">Learn more </a>
+        </main>
+    )
+}
+
+export default Main
